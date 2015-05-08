16732ece9867ba259921aa4acfd54a88e5a26e15
Mihai Popescu <mihaip2008@gmail.com>
2015-05-08 16:37:05 +0300

    parse pbn file

---
+++ source/PBNParserTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PBNParserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string pbnFile = @"C:\Users\mpopescu\Desktop\sample.txt";
+            var regex = new Regex("[Deal \".\"]");
+            var boards = 0;
+
+            using (var reader = new StreamReader(pbnFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!line.StartsWith("[Deal ")) continue;
+
+                    var start = line.IndexOf('"');
+                    var end = line.LastIndexOf('"');
+
+                    var pbnDeal = line.Substring(start + 1, end - start - 1);
+
+                    if (pbnDeal.Length > 0)
+                    {
+                        Console.WriteLine(pbnDeal);
+                        boards++;
+                    }
+                        
+                }
+            }
+
+            Console.WriteLine("{0} Boards", boards);
+            Console.ReadLine();
+        }
+    }
+}
