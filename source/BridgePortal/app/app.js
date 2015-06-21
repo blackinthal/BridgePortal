9336e666bc35435be91a98fcadb2b2c641807a09
mihaip150190 <mihaip2008@gmail.com>
2015-06-22 01:36:20 +0300

    no message

---
+++ source/BridgePortal/app/app.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+    var app = angular.module('BridgePortal', ['ui.router']);
+
+    app.config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'landing.html'
+            })
+            .state('viewDeal', {
+                url: '/viewDeal',
+                templateUrl: 'app/deals/viewDeal.html'
+            });
+    });
+})();
