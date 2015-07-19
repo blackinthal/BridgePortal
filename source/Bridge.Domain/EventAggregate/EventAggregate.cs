e4bf85dd83d12eead02a90fe3a97a7117668b430
mihaip150190 <mihaip2008@gmail.com>
2015-07-19 14:31:16 +0300

    Configure elmah logging

--- source/Bridge.Domain/EventAggregate/EventAggregate.cs
+++ source/Bridge.Domain/EventAggregate/EventAggregate.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Bridge.Domain.EventAggregate.Commands;
 using Bridge.Domain.Models;
 using Domain.Contracts;
